1815284228d47000e43c4a64e2f5bb61fd0cf7da
PabloDRezende <pablo.r.diego@gmail.com>
2022-04-06 16:50:20 -0300

    Início Projeto.

---
+++ src/main/java/com/api/gamingcontrol/GamingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.gamingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingControlApplication.class, args);
+	}
+
+}
